4b9f9590d7d3de2bc59f6d61574b0694eecc231c
_Avery <itzaverytv@gmx.net>
2020-03-22 11:34:37 +0100

    updated privacy policy and added to routing, removed icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ImpressumComponent} from './impressum/impressum.component';
+import {DatenschutzComponent} from './datenschutz/datenschutz.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'impressum', component: ImpressumComponent}
+  {path: 'impressum', component: ImpressumComponent},
+  {path: 'datenschutz', component: DatenschutzComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
