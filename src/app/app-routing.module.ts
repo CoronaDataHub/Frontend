b5fa5b45f6901df058bd769cdeaa3f05a9f858b9
MrCodingMen <info@mrcodingmen.de>
2020-03-25 18:08:13 +0100

    Add credit-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {ChangelogComponent} from './components/changelog/changelog.component';
 import {ImpressumComponent} from './components/impressum/impressum.component';
 import {DatenschutzComponent} from './components/datenschutz/datenschutz.component';
 import {HomeComponent} from './components/home/home.component';
+import {CreditsComponent} from './components/credits/credits.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'changelog', component: ChangelogComponent},
+  {path: 'credits', component: CreditsComponent},
   {path: 'impressum', component: ImpressumComponent},
   {path: 'datenschutz', component: DatenschutzComponent}
 ];
